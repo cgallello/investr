04575ff13731fd477d688f793e74f30418a75103
unknown <cgallello@.olin.edu>
2011-03-02 23:22:38 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Investwithme::Application.config.secret_token = '8000f46208a464b8df556cb9ae6ef486872ad6fdccf3dedf1f2e188db62d7e0129b1cee34b7e4c4b4506f5798628580353868acacf3f5cc5f7b6b792c2335085'
